7f2418ea52c5366dd5d0176581df07ecbbd326b4
Erik Shukhyan <shukhyan1998@gmail.com>
2024-07-20 13:04:17 +0400

    Base ASC and attribute set

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Erik Shukhyan
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Erik Shukhyan
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
